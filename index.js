914d65c794cfa33a8803621e7ce9b4b62ecad52d
Hubert OG <hubert@orlikarnia.com>
2014-05-22 13:19:51 +0200

    4. Notes are visible

--- index.js
+++ index.js
@@ -8,5 +8,13 @@ if(Meteor.isClient) {
     return Notes.find();
   };
 
+  Template.area.x = function() {
+    return this.x * $('.area').width();
+  };
+
+  Template.area.y = function() {
+    return this.y * $('.area').height();
+  };
+
 
 }
